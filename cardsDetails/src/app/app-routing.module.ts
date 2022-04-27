e495820f6d0ea0f80ac46dcbb92eddda6d102130
neha <neha.kumari732@gmail.com>
2022-04-27 13:02:52 +0530

    add materials

--- cardsDetails/src/app/app-routing.module.ts
+++ cardsDetails/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
+import { CardsComponent } from './cards/cards.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'card',component:CardsComponent},
+  {path:'',redirectTo:'card',pathMatch:'full'},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- cardsDetails/src/app/app.module.ts
+++ cardsDetails/src/app/app.module.ts
@@ -3,14 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { CardsComponent } from './cards/cards.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatCardModule} from '@angular/material/card';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
